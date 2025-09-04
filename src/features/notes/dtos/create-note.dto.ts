2bdb1270617c18b145468339c3a9881e8cf82eca
Drummonds <drummond05@proton.me>
2025-09-04 18:30:28 -0300

    feat: enhance createNoteSchema validation with title and description length constraints

--- src/features/notes/dtos/create-note.dto.ts
+++ src/features/notes/dtos/create-note.dto.ts
@@ -2,8 +2,14 @@ import z from "zod";
 
 export const createNoteSchema = z
   .object({
-    title: z.string().min(3, "O título deve ter no mínimo 3 caracteres."),
-    description: z.string(),
+    title: z
+      .string()
+      .min(3, "O título deve ter no mínimo 3 caracteres.")
+      .max(50, "O título deve ter no máximo 50 caracteres."),
+    description: z
+      .string()
+      .min(3, "A descrição deve ter no mínimo 3 caracteres.")
+      .max(500, "A descrição deve ter no máximo 500 caracteres."),
     importance: z.enum(["baixo", "medio", "alto"]),
   })
   .strict();
